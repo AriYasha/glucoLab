3730b9d76e22960f9722ea373e99312daf51ee6d
Iryna <irinanootehnika@mail.ru>
2020-04-09 16:14:48 +0300

    some changes

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    public Button updatePortsButton;    
+    @FXML
+    public Button openPortButton;
+    public ChoiceBox portChoiceBox;
+    public ChoiceBox speedChoiceBox;
+    public ChoiceBox parityChoiceBox;
+    public ChoiceBox stopBitsChoiceBox;
+    public ChoiceBox sizeCharChoiceBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void openPort(ActionEvent actionEvent) {
+    }
+
+    public void refresh(ActionEvent actionEvent) {
+    }
 }
